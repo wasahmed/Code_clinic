8b20135cbf71ef68492223cbe44f6746bc23fdf1
Waseem <wasahmed@student.wethinkcode.co.za>
2020-11-05 14:55:44 +0200

    x

--- feature_a.py
+++
@@ -1,3 +0,0 @@
-
-def print_a():
-    print("This is feature a")
--- main.py
+++
@@ -1,6 +0,0 @@
-import feature_a
-
-
-if __name__ == "__main__":
-    print("HI AND WELCOME TO CODE CLINIC")
-    feature_a.print_a()

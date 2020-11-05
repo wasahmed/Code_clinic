a51e844c564c68ec5f04f101397d3c190393ba9b
Waseem <wasahmed@student.wethinkcode.co.za>
2020-11-05 16:11:35 +0200

    this is a new branch

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import abigail
 import ace
 import A
 import test
+import new
 
 if __name__ == '__main__':
     abigail.say_my_name()
     ace.say_my_name()
     A.print_a()
     test.print_test()
+    new.print_new()
---
+++ new.py
@@ -0,0 +1,3 @@
+
+def print_new():
+    print("This is a new branch")
